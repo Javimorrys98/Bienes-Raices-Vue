7e982a0051c4a70006e5f2eb5140e17be3b193a7
Javimorrys98 <jtomeroldan@gmail.com>
2024-03-17 20:54:47 +0100

    wip2

---
+++ src/composables/useImage.js
@@ -0,0 +1,6 @@
+export default function useImage(){
+    
+    return{
+
+    }
+}
--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,11 +1,81 @@
 <script setup>
+import { useForm, useField } from 'vee-validate';
+import { collection, addDoc } from 'firebase/firestore';
+import { useFirestore } from 'vuefire';
+import { useRouter } from 'vue-router';
+import { validationSchema, imageSchema } from '@/validation/propiedadSchema'
 
+const items = [1, 2, 3, 4, 5]
+
+const router = useRouter()
+
+const db = useFirestore()
+
+const { handleSubmit } = useForm({
+    validationSchema: {
+        ...validationSchema,
+        ...imageSchema
+    }
+})
+
+const titulo = useField('titulo')
+const imagen = useField('imagen')
+const precio = useField('precio')
+const habitaciones = useField('habitaciones')
+const wc = useField('wc')
+const estacionamiento = useField('estacionamiento')
+const descripcion = useField('descripcion')
+const piscina  = useField('piscina', null, {
+    initialValue: false
+})
+
+const submit = handleSubmit(async(values) => {
+    const {imagen, ...propiedad} = values
+
+    const docRef = await addDoc(collection(db, "propiedades"), {
+        ...propiedad
+    });
+    if(docRef.id) {
+        router.push({name: 'admin-propiedades'})
+    }
+})
 </script>
 
 <template>
-    <h2 class="text-center, text-h3 my-5 font-weight-bold">Nueva propiedad</h2>
+    <v-card max-width="800" flat class="mx-auto my-10">
+        <v-card-title class="text-h4 font-weight-bold" tag="h3">
+            Nueva propiedad
+        </v-card-title>
+        <v-card-subtitle class="text-h5 py-5">
+            Crea una nueva propiedad completando el formulario
+        </v-card-subtitle>
+    </v-card>
+    <v-form class="mt-10">
+        <v-text-field v-model="titulo.value.value" :error-messages="titulo.errorMessage.value" class="mb-5"
+            label="Título de la propiedad" />
+        <v-file-input v-model="imagen.value.value" :error-messages="imagen.errorMessage.value" accept="image/jpeg"
+            label="Fotografía" prepend-icon="mdi-camera" class="mb-5" />
+        <v-text-field v-model="precio.value.value" :error-messages="precio.errorMessage.value" class="mb-5"
+            label="Precio" />
+        <v-row>
+            <v-col cols="12" md="4">
+                <v-select v-model="habitaciones.value.value" :error-messages="habitaciones.errorMessage.value"
+                    label="Habitaciones" class="mb-5" :items="items" />
+            </v-col>
+            <v-col cols="12" md="4">
+                <v-select v-model="wc.value.value" :error-messages="wc.errorMessage.value" label="Baños" class="mb-5"
+                    :items="items" />
+            </v-col>
+            <v-col cols="12" md="4">
+                <v-select v-model="estacionamiento.value.value" :error-messages="estacionamiento.errorMessage.value"
+                    label="Lugares de estacionamiento" class="mb-5" :items="items" />
+            </v-col>
+        </v-row>
+        <v-textarea v-model="descripcion.value.value" :error-messages="descripcion.errorMessage.value" class="mb-5"
+            label="Descripción" />
+        <v-checkbox v-model="piscina.value.value" label="Piscina" />
+        <v-btn color="pink-accent-4" block @click="submit">Agregar propiedad</v-btn>
+    </v-form>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
